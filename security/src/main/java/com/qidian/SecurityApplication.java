a7a413a443c9b72e387ba6b8d05ec2c335dd7ba2
git <1771615183@qq.com>
2018-11-27 21:06:23 +0800

    服务提供者

--- security/src/main/java/com/qidian/SecurityApplication.java
+++ security/src/main/java/com/qidian/SecurityApplication.java
@@ -2,9 +2,11 @@ package com.qidian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+@EnableDiscoveryClient 
 @EnableTransactionManagement //开启扫描
-@SpringBootApplication
+@SpringBootApplication //，声明这个一个服务提供者。
 public class SecurityApplication    {
 
 	public static void main(String[] args) {
