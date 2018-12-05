788d7a5648041cbe8362aadc87109fbb9971e5fa
zhongyou-1 <1771615183@qq.com>
2018-12-05 21:47:27 +0800

    测试提供

--- security/src/main/java/com/qidian/SecurityApplication.java
+++ security/src/main/java/com/qidian/SecurityApplication.java
@@ -3,12 +3,14 @@ package com.qidian;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableDiscoveryClient  //，声明这个一个服务提供者。
 @EnableTransactionManagement //开启扫描
 @SpringBootApplication
+@EnableFeignClients // 声明这个一个服务消费者。(//启用feign进行远程调用)
 public class SecurityApplication    {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
