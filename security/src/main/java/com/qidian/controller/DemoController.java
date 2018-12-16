306851dbe4a76d9026032c971d26a70de6689e24
zhongyou-1 <1771615183@qq.com>
2018-12-16 11:14:24 +0800

    合并

--- security/src/main/java/com/qidian/controller/DemoController.java
+++ security/src/main/java/com/qidian/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.qidian.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -7,21 +8,22 @@ import javax.persistence.PersistenceContext;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.qidian.entity.BootUser;
+import com.qidian.entity.User;
 import com.qidian.entity.comm.Result;
 import com.qidian.entity.comm.Results;
-
 
 /**
  * 
  * @title 测试类
  * @author Xrt rong tao
  * @version 1.0.0
- * @since jdk1.8
+ * @since jdk1.8  d
  * @创建时间：2018年11月27日下午7:40:15 @功能描述：
  */
 @RestController
@@ -31,48 +33,47 @@ public class DemoController {
 	protected static Logger logger = LoggerFactory.getLogger(DemoController.class);
 	@PersistenceContext
 	private EntityManager manager;
-	
-//	@Autowired
-//	private com.qidian.dao.UserDAO userDAO;
+
+	@Autowired
+	private com.qidian.dao.UserDAO userDAO;
 
 	@RequestMapping("/hello")
 	@ResponseBody
 	public Result hello() {
 		logger.debug("访问getUserByName,Name={}");
-		List<BootUser> list = manager.createQuery("select o from BootUser o ",BootUser.class).getResultList();
-		
-		 
+		List<User> list = manager.createQuery("select * from Note", User.class).getResultList();
 		logger.error("----------info," + list.size());
 		return  Results.SUCCESS.build(list);
 	}
 
-//	@RequestMapping("/initdata")
-//	@ResponseBody
-//	public Result initData() {
-//        
-//		BootUser user = new BootUser();
-//        user.setName("管理员");
-//        user.setLoginName("admin");
-//        userDAO.insert(user);
-//        Assert.notNull(user.getId(),"用户ID不能为空！");
-//    	return  Results.SUCCESS.build();
-//	}
-//	
-// 
-//	@RequestMapping("/update")
-//	@ResponseBody
-//	public Result update(String key ,String updateName) {
-//        
-//        User user = manager.find(User.class, Long.parseLong(key));
+	@RequestMapping("/initdata")
+	@ResponseBody
+	public Result initData() {
+        
+       /* User user = new User();
+        user.setName("管理员");
+        user.setLoginName("admin");
+        userDAO.insert(user);
+        Assert.notNull(user.getId(),"用户ID不能为空！");*/
+    	return  Results.SUCCESS.build();
+	}
+	
+ 
+	@RequestMapping("/update")
+	@ResponseBody
+	public Result update(String key ,String updateName) {
+        
+        User user = manager.find(User.class, Long.parseLong(key));
 //        user.setName(updateName);
-//        userDAO.update(user);
-//    	return  Results.SUCCESS.build();
-//	}
-//	@RequestMapping("/remove")
-//	@ResponseBody
-//	public Result remove(String key ) {
-//        User user = manager.find(User.class, Long.parseLong(key));//指定key
-//        userDAO.remove(user);
-//    	return  Results.SUCCESS.build();
-//	}
+        userDAO.update(user);
+    	return  Results.SUCCESS.build();
+	}
+	
+	@RequestMapping("/remove")
+	@ResponseBody
+	public Result remove(String key ) {
+        User user = manager.find(User.class, Long.parseLong(key));//指定key
+        userDAO.remove(user);
+    	return  Results.SUCCESS.build();
+	}
 }
--- security/src/main/java/com/qidian/server/DemoServer.java
+++ security/src/main/java/com/qidian/server/DemoServer.java
@@ -1,6 +1,7 @@
 package com.qidian.server;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
